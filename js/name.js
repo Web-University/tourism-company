7e83ab7442564cec09ad02271710de6385ee890c
unknown <maksymchukmaxym@gmail.com>
2022-05-22 21:08:43 +0300

    footer and some changes for site adaptation

--- js/name.js
+++ js/name.js
@@ -1,2 +1,14 @@
-var name = prompt("Ваше ім'я?", "");
-alert(name);
+window.addEventListener('DOMContentLoaded', () => {
+    let name = localStorage.getItem("name")
+    if (!name){
+        setTimeout(() => {
+            name = prompt("Як ми можемо до вас звертатись?", "")
+            if(name){
+                localStorage.setItem("name", name);
+                document.querySelector("#UserName").innerHTML = `Ми вже чекаємо на ваш дзвінок : ${name.toUpperCase()}`;
+            }
+        }, 2000)
+    }
+    document.querySelector("#UserName").innerHTML = `Ми вже чекаємо на ваш дзвінок : ${name.toUpperCase()||""}`;
+    toUpperCase
+});
